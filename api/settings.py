1a1692be6e1b9d498945ae4662b44625b97e0e81
storrellas <storrellas@gmail.com>
2025-11-23 12:29:22 +0100

    feat: remove sqlite

--- api/settings.py
+++ api/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
